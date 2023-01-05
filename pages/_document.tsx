8a6c7b3e1bfbe8f5a01d150cd38deea8816ea338
Liam Costello <liam@sesamecare.com>
2023-01-05 17:40:45 +0100

    Host fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,7 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,700;1,700&family=Merriweather&family=Poppins&display=swap"
-                    rel="stylesheet"/>
-            </Head>
+            <Head/>
             <body>
             <Main/>
             <NextScript/>
