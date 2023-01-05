913bf92c5ead266c95438a4349e8edb4ba4e1a32
Liam Costello <liam@sesamecare.com>
2023-01-05 11:55:23 +0100

    CSS is the devil

--- pages/navbar.tsx
+++ pages/navbar.tsx
@@ -2,13 +2,17 @@ import styles from '../styles/Navbar.module.css'
 
 export default function NavBar() {
     return (
-        <div className={styles.navWrapper}>
-            <nav className={`${styles.navCommon} ${styles.navMain}`}>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="https://www.linkedin.com/in/lrjcostello/">LinkedIn</a></li>
-                    <li><a href="https://github.com/lolletsoc">GitHub</a></li></ul>
-            </nav>
+        <div className={styles.navFlex}>
+            <div className={styles.navDiv}>
+                <nav className={`${styles.navCommon} ${styles.navMain}`}>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="https://www.linkedin.com/in/lrjcostello/">LinkedIn</a></li>
+                        <li><a href="https://github.com/lolletsoc">GitHub</a></li>
+                    </ul>
+                </nav>
+            </div>
+
         </div>
     )
 }
