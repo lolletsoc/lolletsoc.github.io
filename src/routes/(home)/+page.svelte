0db4eaa21679f9283ff0444841b76643b53bc65e
Liam Costello <liam@dehook.io>
2023-08-09 18:26:58 +0200

    CV in better place

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -10,17 +10,11 @@
             <h1>Liam is a <span class="font-bold">principal engineer</span> and <span class="font-bold">architect</span> <br/> building products,
             high-performance <br/> systems and teams.</h1>
         </span>
+
         <p class="font-poppins">I'm a seasoned engineer with 10+ years of experience in backend development,
             infrastructure, architectural design and leading engineering organisations.
             I focus on clean well thought out patterns, ensuring consistency across the stack and
             ultimately reducing the need for significant refactoring in the future.
-        </p>
-
-        <h2 class="font-merriweather font-bold">Want to <i>hire me</i> or fancy a chat?</h2>
-        <p>
-            I'm available for hire, consultation and mentoring. <br/>
-            Contact me on <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                             href="mailto:liam@lolletsoc.com">liam@lolletsoc.com</a>
         </p>
 
         <nav>
@@ -31,6 +25,13 @@
                 <a href="{base}/liam_costello_cv.pdf" rel="nofollow">Download CV</a>
             </button>
         </nav>
+
+        <h2 class="font-merriweather font-bold">Want to <i>hire me</i> or fancy a chat?</h2>
+        <p>
+            I'm available for hire, consultation and mentoring. <br/>
+            Contact me on <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                             href="mailto:liam@lolletsoc.com">liam@lolletsoc.com</a>
+        </p>
 
     </div>
     <Img class="rounded-2xl ml-6 md:block hidden" src={me} alt="Picture of me"/>
