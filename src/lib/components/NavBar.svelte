6ef9fe2f5e06bb4021c99b597d7ee94cf48f2d8b
Liam Costello <liam@dehook.io>
2023-08-09 18:17:08 +0200

    flexy flex

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -4,7 +4,7 @@
 
 <div class="mx-auto flex justify-center bg-black flex-col items-center">
     <nav>
-        <ul class="btn-group [&_li]:px-4 text-white">
+        <ul class="btn-group [&_li]:px-4 text-white flex">
             <li><a class="btn" href="{base}/">Home</a></li>
             <li><a class="btn" rel="external" href="https://www.linkedin.com/in/lrjcostello/">LinkedIn</a></li>
             <li><a class="btn" rel="external" href="https://github.com/lolletsoc">GitHub</a></li>
